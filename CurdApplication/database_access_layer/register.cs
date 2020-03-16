46dae347abaf2d37f949c68abd28de6926e1bedc
EbrahimDeen <ebrahimdeen96@gmail.com>
2020-03-16 12:26:31 -0700

    First Commit

---
+++ CurdApplication/database_access_layer/register.cs
@@ -0,0 +1,20 @@
+﻿namespace CurdApplication.database_access_layer
+{
+    public class register
+
+    {
+
+        public int Sr_no { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+    }
+}
